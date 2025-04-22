4e75f97b50ee8b9e54245a3fc3288c2254e5af08
DiemTV2 <vietdiemtran@gmail.com>
2025-04-22 11:40:54 +0700

    build

---
+++ docs/assets/skeleton-Bzpb_Sqk.js
@@ -0,0 +1,6 @@
+import{f as a,j as c,x as t}from"./index-Dj3cS6za.js";/**
+ * @license lucide-react v0.488.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]],r=a("user",n);function d({className:e,...s}){return c.jsx("div",{className:t("animate-pulse rounded-md bg-muted",e),...s})}export{d as S,r as U};
